c6234286ab1e547657228b41d4c2a6fd7da64e0c
Carlos <l19070065@valladolid.tecnm.mx>
2024-04-14 17:40:03 -0600

    Primer commit

---
+++ PuntoVentaKX/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace PuntoVentaKX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFolio = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtAdultos = new System.Windows.Forms.TextBox();
+            this.txtMenores = new System.Windows.Forms.TextBox();
+            this.txtInfantes = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtCliente = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPaquete = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtReferencia = new System.Windows.Forms.TextBox();
+            this.btbSolicitarNombre = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnSolicitarHoraServidor = new System.Windows.Forms.Button();
+            this.txtHoraServidor = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtFolio
+            // 
+            this.txtFolio.Location = new System.Drawing.Point(106, 71);
+            this.txtFolio.Name = "txtFolio";
+            this.txtFolio.Size = new System.Drawing.Size(100, 20);
+            this.txtFolio.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Folio de reserva:";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(212, 71);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // txtAdultos
+            // 
+            this.txtAdultos.Enabled = false;
+            this.txtAdultos.Location = new System.Drawing.Point(106, 158);
+            this.txtAdultos.Name = "txtAdultos";
+            this.txtAdultos.Size = new System.Drawing.Size(100, 20);
+            this.txtAdultos.TabIndex = 3;
+            // 
+            // txtMenores
+            // 
+            this.txtMenores.Enabled = false;
+            this.txtMenores.Location = new System.Drawing.Point(293, 158);
+            this.txtMenores.Name = "txtMenores";
+            this.txtMenores.Size = new System.Drawing.Size(100, 20);
+            this.txtMenores.TabIndex = 4;
+            // 
+            // txtInfantes
+            // 
+            this.txtInfantes.Enabled = false;
+            this.txtInfantes.Location = new System.Drawing.Point(512, 158);
+            this.txtInfantes.Name = "txtInfantes";
+            this.txtInfantes.Size = new System.Drawing.Size(100, 20);
+            this.txtInfantes.TabIndex = 5;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Enabled = false;
+            this.txtPrecio.Location = new System.Drawing.Point(106, 208);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 6;
+            // 
+            // txtCliente
+            // 
+            this.txtCliente.Enabled = false;
+            this.txtCliente.Location = new System.Drawing.Point(283, 212);
+            this.txtCliente.Name = "txtCliente";
+            this.txtCliente.Size = new System.Drawing.Size(329, 20);
+            this.txtCliente.TabIndex = 7;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Enabled = false;
+            this.txtTelefono.Location = new System.Drawing.Point(106, 261);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(182, 20);
+            this.txtTelefono.TabIndex = 8;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Enabled = false;
+            this.txtCorreo.Location = new System.Drawing.Point(421, 261);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(191, 20);
+            this.txtCorreo.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Adultos:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(237, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Menores:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(458, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Infantes:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(60, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Precio:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(50, 118);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Paquete:";
+            // 
+            // txtPaquete
+            // 
+            this.txtPaquete.Enabled = false;
+            this.txtPaquete.Location = new System.Drawing.Point(106, 115);
+            this.txtPaquete.Name = "txtPaquete";
+            this.txtPaquete.Size = new System.Drawing.Size(506, 20);
+            this.txtPaquete.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(237, 215);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Cliente:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(50, 264);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Telefono:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(375, 264);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(41, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Correo:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(40, 312);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(62, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Referencia:";
+            // 
+            // txtReferencia
+            // 
+            this.txtReferencia.Enabled = false;
+            this.txtReferencia.Location = new System.Drawing.Point(106, 309);
+            this.txtReferencia.Name = "txtReferencia";
+            this.txtReferencia.Size = new System.Drawing.Size(506, 20);
+            this.txtReferencia.TabIndex = 19;
+            // 
+            // btbSolicitarNombre
+            // 
+            this.btbSolicitarNombre.Location = new System.Drawing.Point(13, 13);
+            this.btbSolicitarNombre.Name = "btbSolicitarNombre";
+            this.btbSolicitarNombre.Size = new System.Drawing.Size(95, 23);
+            this.btbSolicitarNombre.TabIndex = 22;
+            this.btbSolicitarNombre.Text = "Nombre";
+            this.btbSolicitarNombre.UseVisualStyleBackColor = true;
+            this.btbSolicitarNombre.Click += new System.EventHandler(this.btbSolicitarNombre_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Enabled = false;
+            this.txtNombre.Location = new System.Drawing.Point(114, 16);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(173, 20);
+            this.txtNombre.TabIndex = 23;
+            // 
+            // btnSolicitarHoraServidor
+            // 
+            this.btnSolicitarHoraServidor.Location = new System.Drawing.Point(353, 14);
+            this.btnSolicitarHoraServidor.Name = "btnSolicitarHoraServidor";
+            this.btnSolicitarHoraServidor.Size = new System.Drawing.Size(95, 23);
+            this.btnSolicitarHoraServidor.TabIndex = 24;
+            this.btnSolicitarHoraServidor.Text = "Hora servidor";
+            this.btnSolicitarHoraServidor.UseVisualStyleBackColor = true;
+            this.btnSolicitarHoraServidor.Click += new System.EventHandler(this.btnSolicitarHoraServidor_Click);
+            // 
+            // txtHoraServidor
+            // 
+            this.txtHoraServidor.Enabled = false;
+            this.txtHoraServidor.Location = new System.Drawing.Point(454, 16);
+            this.txtHoraServidor.Name = "txtHoraServidor";
+            this.txtHoraServidor.Size = new System.Drawing.Size(174, 20);
+            this.txtHoraServidor.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 348);
+            this.Controls.Add(this.txtHoraServidor);
+            this.Controls.Add(this.btnSolicitarHoraServidor);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btbSolicitarNombre);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtReferencia);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtPaquete);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtCliente);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtInfantes);
+            this.Controls.Add(this.txtMenores);
+            this.Controls.Add(this.txtAdultos);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFolio);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFolio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txtAdultos;
+        private System.Windows.Forms.TextBox txtMenores;
+        private System.Windows.Forms.TextBox txtInfantes;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtCliente;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPaquete;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtReferencia;
+        private System.Windows.Forms.Button btbSolicitarNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnSolicitarHoraServidor;
+        private System.Windows.Forms.TextBox txtHoraServidor;
+    }
+}
+
---
+++ PuntoVentaKX/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuntoVentaKX
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btbSolicitarNombre_Click(object sender, EventArgs e)
+        {
+            var reservacion = new ServiceReferenceKX.WebServicesKXSoapClient();
+            string name = reservacion.DevolverNombre();
+
+
+            txtNombre.Text = name;
+        }
+
+
+        private void btnSolicitarHoraServidor_Click(object sender, EventArgs e)
+        {
+            var reservacion = new ServiceReferenceKX.WebServicesKXSoapClient();
+            string hora = reservacion.HoraServidor();
+
+
+            txtHoraServidor.Text = hora;    
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            var reservacion = new ServiceReferenceKX.WebServicesKXSoapClient();
+            int folio = int.Parse(txtFolio.Text);
+            var reserva = reservacion.BuscarReserva(folio);
+            
+            txtAdultos.Text = reserva[1];
+            txtPaquete.Text = reserva[2];
+            txtMenores.Text = reserva[3];
+            txtPrecio.Text = "$ " + reserva[4];
+            txtInfantes.Text = reserva[5];
+            txtCliente.Text = reserva[6];
+            txtReferencia.Text = reserva[7];
+            txtCorreo.Text = reserva[8];
+            txtTelefono.Text = reserva[9];
+
+        }
+
+    }
+}
